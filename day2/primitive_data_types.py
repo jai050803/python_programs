15560cca48367cc86080bcdcb0d8deeedd46150c
jai <sjai5803@gmail.com>
2024-01-19 00:00:35 +0530

    day 2 of python programming

---
+++ day2/primitive_data_types.py
@@ -0,0 +1,22 @@
+#string 
+
+print("hello world")
+
+print("hello"[0]) #print the first letter of our string (character at index 0)
+
+print("123" + "456") #concatenate the string
+
+#integers
+
+print(123 + 456) # 123 + 456 integers and give us the addition result of two numbers
+
+print(123_456) # 123456 integer here underscore work same as commas work in our numbers while writing.
+
+#float 
+
+print(3.14) #floating point number with decimal points
+
+#boolean values
+
+print(True)
+print(False)
