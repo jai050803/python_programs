30f7e7c1f1d44c792fc7aaff3d4e5e78d193cc98
jai <sjai5803@gmail.com>
2024-01-19 20:56:18 +0530

    roller coaster ride

--- day3/rollercoster_ride.py
+++ day3/rollercoster_ride.py
@@ -1,7 +1,11 @@
 print("\n.....roller coaster ride ...... \n")
 height = int(input("enter your height in cm : "))
+age = int(input("enter your age : "))
 
-if height > 120:
-    print("you can ride the rollercoaster")
+if height >= 120:
+    if age >= 18:
+        print("you can ride the rollercoaster and you need to pay 100 rupees")
+    else:
+        print("you can ride the rollercoaster and you need to pay 50 rupees")
 else:
     print("you can't ride the rollercoaster")
