71d9e6b41b1d77c2c1f21b825b327cee4cd3987b
jai <sjai5803@gmail.com>
2024-01-19 20:50:44 +0530

    to see if you can ride the rollercoaster or not

---
+++ day3/rollercoster_ride.py
@@ -0,0 +1,7 @@
+print("\n.....roller coaster ride ...... \n")
+height = int(input("enter your height in cm : "))
+
+if height > 120:
+    print("you can ride the rollercoaster")
+else:
+    print("you can't ride the rollercoaster")
