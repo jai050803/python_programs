0cf196c25342ed7c936d17e8b2981d6cf7b148ef
jai <sjai5803@gmail.com>
2024-01-21 23:13:42 +0530

    task1 of hangman project done

---
+++ day7/hangman_project.py
@@ -0,0 +1,7 @@
+#task1 - pick a random word from the list of words
+import random
+word = ["hello", "balloon" , "adventure", "bookkeeper", "andaman", "narendra", "delhi"]
+
+chosen_word = random.choice(word)
+
+
