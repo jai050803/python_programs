82e9ec144f82e7ba18033ac8731bebff7b8edd81
jai <sjai5803@gmail.com>
2024-01-22 16:34:19 +0530

    hangman

--- day7/hangman_project.py
+++ day7/hangman_project.py
@@ -1,19 +1,80 @@
 #task1 - pick a random word from the list of words
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word = ["hello", "balloon" , "adventure", "bookkeeper", "andaman", "narendra", "delhi"]
 
 chosen_word = random.choice(word)
-
+print(chosen_word)
 display = []
 for i in range(len(chosen_word)):
     display.append("_")
+for i in chosen_word:
+    while "_" in display:
+        guess = input("\n\n\nGuess a letter : ").lower()
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                display[i] = guess
+        
+            else:
+                for i in range(len(stages)-1,0,-1):
+                    print(stages[i])
 
-#task2 - take input from the user
-guess = input("\n\n\nGuess a letter : ").lower()
 
-#task3 - check if the letter is in the word and if yes replace it in the display list
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display[i] = guess
-
-print(display)
+        
