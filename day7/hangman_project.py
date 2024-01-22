ac928485796d5eb15351fb3b4f77e2af585cfe06
jai <sjai5803@gmail.com>
2024-01-22 11:17:11 +0530

    task 4 completed

--- day7/hangman_project.py
+++ day7/hangman_project.py
@@ -3,19 +3,17 @@ import random
 word = ["hello", "balloon" , "adventure", "bookkeeper", "andaman", "narendra", "delhi"]
 
 chosen_word = random.choice(word)
+
 display = []
 for i in range(len(chosen_word)):
     display.append("_")
-for i in display:
-    print(i, end=" ")
 
 #task2 - take input from the user
-guess = input("\n\n\nGuess a letter :").lower()
+guess = input("\n\n\nGuess a letter : ").lower()
 
+#task3 - check if the letter is in the word and if yes replace it in the display list
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        display[i] = guess
 
-for i in chosen_word:
-    if guess == i:
-        print("correct")
-    else:
-        print("Wrong")
-
+print(display)
