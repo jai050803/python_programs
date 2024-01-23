add999c915ff879d37ba9d65c658bb750cdfd88b
jai <sjai5803@gmail.com>
2024-01-23 22:14:05 +0530

    final task

--- day7/hangman_project.py
+++ day7/hangman_project.py
@@ -58,20 +58,36 @@ stages = ['''
 =========
 ''']
 
+lives = 6
+
 word = ["hello", "balloon" , "adventure", "bookkeeper", "andaman", "narendra", "delhi"]
-
-chosen_word = random.choice(word)
-print(chosen_word)
-display = []
-for i in range(len(chosen_word)):
-    display.append("_")
-for i in chosen_word:
-    while "_" in display:
-        guess = input("\n\n\nGuess a letter : ").lower()
-        for i in range(len(chosen_word)):
-            if chosen_word[i] == guess:
-                display[i] = guess
-
-        
 end_of_game = False
+chosen_word = random.choice(word)
+word_length = len(chosen_word)
+print(f'Pssst, the solution is {chosen_word}.')
+display = []
+
+for _ in range(word_length):
+  display += "_"
+
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+    
+  if guess not in chosen_word:
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")
+            
+  print(f"{' '.join(display)}")
+    
+  if "_" not in display:
+    end_of_game = True
+    print("You win.")
         
+  print(stages[lives])
+      
