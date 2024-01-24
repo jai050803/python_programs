c9123bb7eb68a72856ae06420269777f8cc03aae
jai <sjai5803@gmail.com>
2024-01-24 23:22:33 +0530

    function with input

--- day8/functions.py
+++ day8/functions.py
@@ -3,3 +3,11 @@ def greet():
     print(f"hello {input("type your name :")}")
 
 greet()
+
+#function that allows the input
+
+def greet_with_name(name):
+    print(f"hello {name}")
+    print("how are you doing today")
+    
+greet_with_name("jai") #take one input for our function
