3f34c47aa2a622f004c3222a08ead892c470eb6f
jai <sjai5803@gmail.com>
2024-01-24 23:26:33 +0530

    function with more than one input

--- day8/functions.py
+++ day8/functions.py
@@ -11,3 +11,8 @@ def greet_with_name(name):
     print("how are you doing today")
     
 greet_with_name("jai") #take one input for our function
+
+def greet_with(name,area):
+    print(f"hello {name} from {area}")
+
+greet_with("jai","delhi")
