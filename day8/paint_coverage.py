8eaab648c4cca425470895696c415d0fe7f4d080
jai <sjai5803@gmail.com>
2024-01-24 23:41:57 +0530

    paint_coverage exercise

--- day8/paint_coverage.py
+++ day8/paint_coverage.py
@@ -1,4 +1,13 @@
 import math
 
 def paint_calc(height,width,coverage):
-    pass
+    paint_required = math.ceil((height*width)/coverage)
+    return paint_required
+
+
+height = float(input("enter height of wall :"))
+width = float(input("enter width of wall :"))
+cover = float(input("enter coverage of wall :"))
+
+paint = paint_calc(height,width,cover)
+print(f"you will need {paint} cans of paint")
