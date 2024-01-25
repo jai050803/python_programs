1e5d82f4979bd0a2b2df3240a884d92e26c4bdba
jai <sjai5803@gmail.com>
2024-01-25 23:25:22 +0530

    encryption program

--- day8/cesar_cipher.py
+++ day8/cesar_cipher.py
@@ -1,3 +1,20 @@
 #project
 
 #part 1 - encryption
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text,shift):
+    for char in text:
+        pos = text.index(char) #initial position of character
+        new_pos = pos + shift
+        if new_pos > len(alphabet):
+            new_pos = new_pos - len(alphabet)
+        new_char = alphabet[new_pos]
+        text = text.replace(char,new_char)
+    print(text)
+    
