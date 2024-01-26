37567bcca67a06632567c1ec4e9860c651f39f73
jai <sjai5803@gmail.com>
2024-01-26 15:41:09 +0530

    encrypt and decrypt functions

--- day8/cesar_cipher.py
+++ day8/cesar_cipher.py
@@ -10,12 +10,38 @@ shift = int(input("Type the shift number:\n"))
 
 def encrypt(text,shift):
     cipher_text = ""
-    new_text = text.replace(" ", "")
-    for char in new_text:
+    for char in text:
+        if char ==" ":
+            cipher_text = cipher_text + " "
+            continue
         position = alphabet.index(char)
         new_index = position + shift
-        cipher_text += alphabet[new_index]
+        if new_index in range(0, len(alphabet)):
+            cipher_text += alphabet[new_index]
+        elif new_index >= len(alphabet):
+            index_n = new_index - len(alphabet)
+            cipher_text += alphabet[index_n]
     
-    return cipher_text
-encrypt(text,shift)
+    print(cipher_text)
+
+def decrypt(text,shift):
+    cipher_text = ""
+    for char in text:
+        if char ==" ":
+            cipher_text = cipher_text + " "
+            continue
+        position = alphabet.index(char)
+        new_index = position - shift
+        # if new_index in range(0, len(alphabet)):
+        cipher_text += alphabet[new_index]
+        # elif new_index >= len(alphabet):
+        #     index_n = new_index + len(alphabet)
+            # cipher_text += alphabet[index_n]
+    print(cipher_text)
+    
+if direction == "encode":
+    encrypt(text,shift)
+    
+if direction == "decode":
+    decrypt(text,shift)
     
