8e025e5f4933105d548e2850cbe117e0f195ab8b
jai <sjai5803@gmail.com>
2024-01-27 12:37:42 +0530

    program to find days in the given month

--- day10/days_in_month.py
+++ day10/days_in_month.py
@@ -1,4 +1,13 @@
 from leapyear import leap_year
 
-a= int(input("enter year :"))
-print(leap_year(a))
+def days_in_month(year,month):
+    month_days=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if month ==2 and leap_year(year) == "leap Year":
+        return 29
+    else:
+        return month_days[month-1]
+
+year = int(input("enter the year : "))
+month = int(input("enter the month : "))
+days = days_in_month(year,month)
+print(f"in given month we have {days} days")
