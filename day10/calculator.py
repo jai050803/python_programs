de021675f97cacacc7c9c3054546d71c7cf98c54
jai <sjai5803@gmail.com>
2024-01-27 14:20:57 +0530

    calculator:

--- day10/calculator.py
+++ day10/calculator.py
@@ -20,26 +20,14 @@ operations = input("Pick the operation from above example : ")
 if operations in dictionary.keys():
     n2 = int(input("enter the second number : "))
     operation_function = dictionary[operations]
-    print(f"{n1} {operations} {n2} = {operation_function(n1,n2)}")
+    answer1 = operation_function(n1,n2)
+    print(f"{n1} {operations} {n2} = {answer1}")
+    operation2 = input("Pick another operation from above: ")
+    n3 = int(input("enter the third number : "))
+    answer2 = operation_function(answer1,n3)
+    print(f"{answer1} {operation2} {n3} = {answer2}")
+    
+    
+    
 else:
     print("invalid operator")
-
-# resume = True
-# while resume == True:
-#     n1 = int(input("enter the first number : "))
-#     print("+ \n- \n* \n/")
-#     op = input("enter the operator : ")
-#     n2 = int(input("enter the second number : "))
-#     if op == "+":
-#         print(add(n1,n2))
-#     elif op == "-":
-#         print(subtract(n1,n2))
-#     elif op == "*":
-#         print(multiply(n1,n2))
-#     elif op == "/":
-#         print(divide(n1,n2))
-#     else:
-#         print("invalid operator")
-#     cont = input("do you want to continue y/n : ").lower()
-#     if cont == "n":
-#         resume == False
