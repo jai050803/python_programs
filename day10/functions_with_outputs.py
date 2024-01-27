281a8e2b7c5fdaff47b4b529e09204e5f1c1ab93
jai <sjai5803@gmail.com>
2024-01-27 12:30:02 +0530

    function with outputs

---
+++ day10/functions_with_outputs.py
@@ -0,0 +1,11 @@
+def lower_Case(text):
+    return text.lower()
+
+def upper_Case(text):
+    return text.upper()
+
+text = input("Type something: ")
+a = lower_Case(text)
+b = upper_Case(text)
+print(a,b)
+    
