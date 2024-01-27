6f3a4f40462da84d1676fd9759f053b9e0a0d084
jai <sjai5803@gmail.com>
2024-01-27 20:32:03 +0530

    calculator project

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,10 +1,36 @@
 def add(n1,n2):
+    """
+    Adds two numbers together and returns the result.
+    """
     return n1+n2
 def subtract(n1,n2):
+    """
+    Subtracts two numbers and returns the result.
+    Parameters:
+    - n1: a number
+    - n2: a number
+    Return type: a number
+    """
     return n1-n2
 def multiply(n1,n2):
+    """
+    Multiplies two numbers and returns the result.
+    Parameters:
+    - n1: first number (int or float)
+    - n2: second number (int or float)
+    Returns:
+    - product of n1 and n2 (int or float)
+    """
     return n1*n2
 def divide(n1,n2):
+    """
+    A function to divide two numbers and return the result.
+    Parameters:
+    - n1: the numerator (dividend)
+    - n2: the denominator (divisor)
+    Return:
+    - The result of dividing n1 by n2
+    """
     return n1/n2
 dictionary = {
     "+":add,
@@ -12,22 +38,24 @@ dictionary = {
     "*":multiply,
     "/":divide
 }
-
-n1 = int(input("enter the first number : "))
-for j in dictionary.keys():
-    print(j)
-operations = input("Pick the operation from above example : ")
-if operations in dictionary.keys():
-    n2 = int(input("enter the second number : "))
-    operation_function = dictionary[operations]
-    answer1 = operation_function(n1,n2)
-    print(f"{n1} {operations} {n2} = {answer1}")
-    operation2 = input("Pick another operation from above: ")
-    n3 = int(input("enter the third number : "))
-    answer2 = operation_function(answer1,n3)
-    print(f"{answer1} {operation2} {n3} = {answer2}")
-    
-    
-    
-else:
-    print("invalid operator")
+def calculator():
+    n1 = float(input("enter the first number : "))
+    for j in dictionary.keys():
+        print(j)
+    should_continue = True
+    while should_continue == True:
+        operations = input("Pick the operation from above example : ")
+        if operations in dictionary.keys():
+            n2 = int(input("enter the second number : "))
+            operation_function = dictionary[operations]
+            answer1 = operation_function(n1,n2)
+            print(f"{n1} {operations} {n2} = {answer1}")
+        else:
+            print("invalid operator")
+        
+        if input("do you want to continue with same answer (y/n)? ") == "y":
+            n1 = answer1
+        else:
+            should_continue = False
+        
+calculator()
