f8d03c977ba9054fc1f8605ac2f01bccbb23ddf5
jai <sjai5803@gmail.com>
2024-01-28 16:13:42 +0530

    debugging programs

--- day13/debugging.py
+++ day13/debugging.py
@@ -1,21 +1,21 @@
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  for i in range(1, 21): #python do not take last value of range function and i will never be equal to 20
-    if i == 20:
-      print("You got it")
-my_function()
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21): #python do not take last value of range function and i will never be equal to 20
+#     if i == 20:
+#       print("You got it")
+# my_function()
 
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
 
-# # Play Computer
+# Play Computer
 # year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
+# if year > 1980 and year <= 1994:
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
