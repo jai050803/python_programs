cd446271d110ce6ea1e6241bb75a7ea51651b471
jai <sjai5803@gmail.com>
2024-01-29 22:18:49 +0530

    day14 - higher lower game

--- day14/main.py
+++ day14/main.py
@@ -5,5 +5,26 @@ print("...Welcome to the Higher or Lower Game....\n")
 
 print("....Guess who has more followers....\n")
 
-name = random.choice(data)
-print(name['name'])
+
+right = True
+while True:
+    name1 = random.choice(data)
+    name2 = random.choice(data)
+    print(f"The competition is between {name1['name']} vs {name2['name']}")
+
+    answer = input("Who has more followers? Type 'a' or 'b' :").lower()
+
+    follower1 = name1['follower_count']
+    follower2 = name2['follower_count']
+    if answer == "a":
+        if follower2 > follower1:
+            print("You are wrong.")
+            right = False
+        else:
+            print("You are right you won this round...")
+    elif answer == "b":
+        if follower2 > follower1:
+            print("You are right. You won this round...")
+        else:
+            print("You are wrong")
+            right = False
