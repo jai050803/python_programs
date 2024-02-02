be6977ccd446ac47d2c5a8b312a256dfc6e062d3
jai <sjai5803@gmail.com>
2024-02-02 22:49:12 +0530

    coffee machine program

--- day15/order.py
+++ day15/order.py
@@ -1,11 +1,11 @@
 from check_function import check
-from resources import resource
+from resources import *
 def order_espresso():
     print("accepted currency : Rs 10, Rs 20, Rs 50")
     insert_money = int(input("insert money : "))
-    if insert_money < resource["espresso"]:
+    if insert_money < espresso["money"]:
         print("not enough money..")
-    elif insert_money == resource["espresso"]:
+    elif insert_money == espresso["money"]:
         print("your espresso is ready")
     else:
-        print(f"your espresso is ready and here is your change of {insert_money-resource['espresso']} rupees")
+        print(f"your espresso is ready and here is your change of {insert_money-espresso['money']} rupees")
