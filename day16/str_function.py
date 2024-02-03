57721628204b4867be023929b0d484a74f2a458f
jai <sjai5803@gmail.com>
2024-02-03 21:27:44 +0530

    str function in python

--- day16/str_function.py
+++ day16/str_function.py
@@ -1,3 +1,7 @@
+# The __str__() function controls what should be returned when the class object is represented as a string.
+
+# If the __str__() function is not set, the string representation of the object is returned:
+
 class Person:
   def __init__(self, name, age):
     self.name = name
