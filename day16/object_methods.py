08db4ec1186c6469b2a570a5301f5977c267a8e5
jai <sjai5803@gmail.com>
2024-02-03 21:23:51 +0530

    object methods

---
+++ day16/object_methods.py
@@ -0,0 +1,12 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#we can have methods in class and we can call them using the object methods.
