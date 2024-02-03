01b026986cd129ff173bc046cb73213abb9df303
jai <sjai5803@gmail.com>
2024-02-03 21:21:38 +0530

    init function in python

--- day16/init_function.py
+++ day16/init_function.py
@@ -1,3 +1,7 @@
+#init is a built-in function in python
+#All classes have a function called __init__(), which is always executed when the class is being initiated.
+#Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+
 class Person:
   def __init__(self, name, age):
     self.name = name
