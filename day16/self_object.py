cee8bd6bdebced69b7337346e52b432f2c2aead3
jai <sjai5803@gmail.com>
2024-02-03 21:26:47 +0530

    self parameter in python

--- day16/self_object.py
+++ day16/self_object.py
@@ -1,3 +1,7 @@
+# The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+
+# It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
+
 class Person:
   def __init__(mysillyobject, name, age):
     mysillyobject.name = name
