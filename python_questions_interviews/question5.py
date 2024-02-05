bb95cf272debd3d94ae4fa3f55799120c92bff1e
jai <sjai5803@gmail.com>
2024-02-05 17:38:44 +0530

    maths module program

--- python_questions_interviews/question5.py
+++ python_questions_interviews/question5.py
@@ -1,4 +1,21 @@
 #write a python program to use math module fucntions
 
 import math
-math.acos(1)
+math.acos(1) #return the arc cosine of a number
+
+math.acosh(1) # returns the inverse hyperbolic cosine of a number
+
+math.asin(1) #return the arc sine of a number
+
+math.asinh(1) #return the inverse hyperboilc cosine of a number
+
+math.atan(1) #return the arc tan of a number
+
+math.atanh(1) #return the inverse hyperbolic tan of a number
+
+math.atan2() #retun the arc tangent of y/x in radians
+
+math.ceil() #return a number nearest to the nearest integer
+
+
+
