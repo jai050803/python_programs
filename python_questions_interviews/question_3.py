a62da9aa49f87cbc904ecc3a11c3b332a7ebed8c
jai <sjai5803@gmail.com>
2024-02-05 17:30:15 +0530

    python program to find if a number is prime or not

--- python_questions_interviews/question_3.py
+++ python_questions_interviews/question_3.py
@@ -1,2 +1,14 @@
 #python program to find if a number is prime or not
 
+
+
+n = int(input("enter the number you want to check : "))
+prime = True
+for i in range(2,n):
+    if n%i == 0:
+        prime = False
+        
+if prime == True:
+    print(f"the number {n} is a prime number")
+else:
+    print(f"The number {n} is not a prime number")
