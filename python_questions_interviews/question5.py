3697e85bf0237ba7afd072a32f0aa97927eba966
jai <sjai5803@gmail.com>
2024-02-05 17:41:46 +0530

    python program to use all the maths functions

--- python_questions_interviews/question5.py
+++ python_questions_interviews/question5.py
@@ -1,21 +1,22 @@
 #write a python program to use math module fucntions
 
 import math
-math.acos(1) #return the arc cosine of a number
+# math.acos(1) #return the arc cosine of a number
 
-math.acosh(1) # returns the inverse hyperbolic cosine of a number
+# math.acosh(1) # returns the inverse hyperbolic cosine of a number
 
-math.asin(1) #return the arc sine of a number
+# math.asin(1) #return the arc sine of a number
 
-math.asinh(1) #return the inverse hyperboilc cosine of a number
+# math.asinh(1) #return the inverse hyperboilc cosine of a number
 
-math.atan(1) #return the arc tan of a number
+# math.atan(1) #return the arc tan of a number
 
-math.atanh(1) #return the inverse hyperbolic tan of a number
+# math.atanh(1) #return the inverse hyperbolic tan of a number
 
-math.atan2() #retun the arc tangent of y/x in radians
+# math.atan2() #retun the arc tangent of y/x in radians
 
-math.ceil() #return a number nearest to the nearest integer
+# math.ceil() #return a number nearest to the nearest integer
 
+print(round((math.log(2)))) #find the log of a number
 
 
