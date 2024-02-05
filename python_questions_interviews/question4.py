a0fe8613a634e9b8bdb5f0a22868c5506a213686
jai <sjai5803@gmail.com>
2024-02-05 17:32:59 +0530

    fizzbuzz program

--- python_questions_interviews/question4.py
+++ python_questions_interviews/question4.py
@@ -1,2 +1,11 @@
 #python program for fizzbuzz program
 
+for i in range(1,101):
+    if i%3 == 0 and i%5 == 0:
+        print("fizzbuzz")
+    elif i%3 == 0:
+        print("fizz")
+    elif i%5 == 0:
+        print("buzz")
+    else:
+        print(i)
