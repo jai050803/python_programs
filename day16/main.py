0ce9afb63c11b5a53812ca586f49e0dff0d91fc0
jai <sjai5803@gmail.com>
2024-02-11 16:34:59 +0530

    day16 object oriented programming

--- day16/main.py
+++ day16/main.py
@@ -3,3 +3,8 @@ import turtle
 import another_module as mod
 
 print(mod.var)
+
+t = turtle.Turtle() #object created with name t  with class Turtle from turtle module
+#paranthesis to initialize or construct the object and save it in variable called t
+
+#you can also import Turtle class as from turtle import Turtle
