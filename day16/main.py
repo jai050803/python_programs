c5011d27d32643543bedc45cb63ed6561d7c0d7a
jai <sjai5803@gmail.com>
2024-02-11 16:38:31 +0530

    screen class from turtle

--- day16/main.py
+++ day16/main.py
@@ -1,10 +1,14 @@
-import turtle
+from turtle import Turtle, Screen
 
 import another_module as mod
 
-print(mod.var)
+# print(mod.var)
 
-t = turtle.Turtle() #object created with name t  with class Turtle from turtle module
+t = Turtle() #object created with name t  with class Turtle from turtle module
 #paranthesis to initialize or construct the object and save it in variable called t
 
 #you can also import Turtle class as from turtle import Turtle
+
+screen = Screen() #object created with name screen with class Screen from turtle module
+
+print(screen.canvheight)
