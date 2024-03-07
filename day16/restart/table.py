47ee946c02c38880d5643cdb3b71252c5f387ac2
jai <sjai5803@gmail.com>
2024-03-07 23:02:17 +0530

    pandas dataframe

--- day16/restart/table.py
+++ day16/restart/table.py
@@ -1,1 +1,16 @@
 import pandas as pd
+
+data = {
+    "Pokemon name" : ["Pikachu", "Squirtle", "Charmender"],
+    "Type" : ["Electric", "Water", "Fire"],
+    "Total" : [90, 60, 80],
+    "HP" : [90, 60, 80],
+    "Attack" : [90, 60, 80],
+    "Defense" : [90, 60, 80],
+    "Sp. Atk" : [90, 60, 80],
+    "Sp. Def" : [90, 60, 80],
+    "Speed" : [90, 60, 80]
+}
+
+df = pd.DataFrame(data)
+print(df)
