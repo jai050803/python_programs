6c3255908620fad6f5479fb0353009fc5cf5295c
jai <sjai5803@gmail.com>
2024-03-07 22:31:10 +0530

    python code

--- day16/pretty_table.py
+++ day16/pretty_table.py
@@ -1,3 +1,12 @@
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
+import numpy as np
+# x = PrettyTable()
 
-x = PrettyTable()
+# #to add rows
+
+x = [1,2,3,4,5]
+width = 0.2
+p = np.arange(len(x))
+p1 = [j+width for j in p]
+print(p)
+print(p1)
