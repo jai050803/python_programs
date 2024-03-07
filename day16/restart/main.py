e2cda83b00d554496ac609a540fe79ecd6b6c578
jai <sjai5803@gmail.com>
2024-03-07 22:41:00 +0530

    turtle

--- day16/restart/main.py
+++ day16/restart/main.py
@@ -1,3 +1,23 @@
 from another_module import value
+from turtle import Turtle, Screen
+# print(value)
 
-print(value)
+tr = Turtle()
+print(tr)
+tr.shape("turtle")
+tr.color("red","green")
+tr.pendown()
+tr.forward(100)
+tr.left(400)
+tr.forward(100)
+tr.right(400)
+tr.forward(100)
+tr.right(400)
+tr.forward(100)
+tr.right(400)
+tr.forward(100)
+tr.right(400)
+
+my_screen = Screen() #creating an object from screen class.. the name of object is my_Screen
+print(my_screen.canvheight) #print canvas height of screen
+my_screen.exitonclick() #myscreen object having an attribute exit on click.
