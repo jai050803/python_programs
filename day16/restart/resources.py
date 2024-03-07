c4254339a5ce959c0f22953d85acd3a4b1109d68
jai <sjai5803@gmail.com>
2024-03-07 23:15:59 +0530

    coffee machine project resources

---
+++ day16/restart/resources.py
@@ -0,0 +1,27 @@
+resource = {
+    "water": 500,
+    "milk" : 300,
+    "coffee" : 50,
+    "money" : 300    
+}
+
+espresso = {
+    "water" : 100,
+    "milk" : 0,
+    "coffee" : 10,
+    "money" : 50
+}
+
+cappuccino = {
+    "water" : 150,
+    "milk" : 50,
+    "coffee" : 15,
+    "money" : 80
+}
+
+latte = {
+    "water" : 200,
+    "milk" : 70,
+    "coffee" : 20,
+    "money" : 50
+}
